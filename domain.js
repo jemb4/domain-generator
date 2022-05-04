f6bc571a192d829034d398068c18b9603f7cc7b2
jemb4 <jesus8.mb@gmail.com>
2022-05-04 12:20:07 +0000

    añadido boton de hack

--- domain.js
+++ domain.js
@@ -2,6 +2,27 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big'];
 let noun = ['jogger','racoon'];
+let hack = false;
+
+
+// Hacer función que se active con el botón:
+
+let btnHack = document.getElementById('btn-hack')
+
+function btnChange(){ //función que cambia el estado del botón
+console.log("ha pas")
+  if(hack){
+    hack = false
+  }
+
+  else{
+    hack = true
+  }
+
+}
+
+btnHack.addEventListener('click', btnChange)
+
 
 //lector de selección
 const selectElement = document.querySelector('#domainSelected');
@@ -9,32 +30,47 @@ const selectElement = document.querySelector('#domainSelected');
 selectElement.addEventListener('change', (event) => {
   const domainId = document.querySelector('#domainGenerated');
 
-  function fooDomain(){ //función que da todos los posibles conjuntos de dominios
-    let res = "";
-    let pronoun = ['the','our'];
-    let adj = ['great', 'big'];
-    let noun = ['jogger','racoon'];
+  function fooDomain(){                   //función que da todos los posibles conjuntos de dominios
+    let resultDomain = "";
     let dom = `${event.target.value}` ;
-    
-    console.log(event.target.value)
 
-    if (dom == "none"){
+    if (dom == "none"){                           //mira si se seleccionó el dominio
       return "Select a domain to get started"
   }
-    else
+
+    else if (dom !== "none" && hack == false)     //si hack no está activo pone todo en minus
       { for(let j in adj){
           for(let i in pronoun){
             for (let k in noun){
-              res += pronoun[i] + adj[j] + noun[k] + dom + "<br>"
+
+              resultDomain += pronoun[i] + adj[j] + noun[k] + dom + "<br>"
+              
+            }
+          }
+        }}
+
+        else                                    //si hack  está activo pone la primera letra de adj y noun en mayus
+        {for(let j in adj){
+          for(let i in pronoun){
+            for (let k in noun){
+
+              resultDomain += pronoun[i] + capitalize(adj[j]) + capitalize(noun[k]) + dom + "<br>"
               
             }
           }
         }}
  
-    return res
+    return resultDomain
   };
   
   let result = fooDomain() 
   domainId.innerHTML = result
 });
 
+
+//funcion mayusculas:
+
+function capitalize(str) {
+  const lower = str.toLowerCase();
+  return str.charAt(0).toUpperCase() + lower.slice(1);
+}
